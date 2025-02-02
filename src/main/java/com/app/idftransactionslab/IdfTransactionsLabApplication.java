d23c1b0aea645656543f4e73f6ec2919d5c0b512
skodin <skodin36@gmail.com>
2025-02-02 20:14:50 +0300

    initial commit refactored

--- src/main/java/com/app/idftransactionslab/IdfTransactionsLabApplication.java
+++ src/main/java/com/app/idftransactionslab/IdfTransactionsLabApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class IdfTransactionsLabApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IdfTransactionsLabApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(IdfTransactionsLabApplication.class, args);
+    }
 
 }
