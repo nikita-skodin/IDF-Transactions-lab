02005e3f56ccc35b01d3f41e61220c865785bfff
skodin <skodin36@gmail.com>
2025-02-02 20:13:31 +0300

    Initial commit

---
+++ src/main/java/com/app/idftransactionslab/IdfTransactionsLabApplication.java
@@ -0,0 +1,13 @@
+package com.app.idftransactionslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdfTransactionsLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdfTransactionsLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/idftransactionslab/IdfTransactionsLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.idftransactionslab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdfTransactionsLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
